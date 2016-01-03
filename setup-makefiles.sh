eff4d20e2b0f0ea8ce50f782f7e2092588c57a9e
W.D <djvoleur@gmail.com>
2016-01-02 21:45:00 -0700

    Update proprietary files list and add naughty files

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -46,6 +46,26 @@ for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
   fi
 done
 
+COUNT=`wc -l naughty-files.txt | awk {'print $1'}`
+DISM=`egrep -c '(^#|^$)' naughty-files.txt`
+COUNT=`expr $COUNT - $DISM`
+for FILE in `egrep -v '(^#|^$)' naughty-files.txt`; do
+  COUNT=`expr $COUNT - 1`
+  if [ $COUNT = "0" ]; then
+    LINEEND=""
+  fi
+  # Split the file from the destination (format is "file[:destination]")
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  if [[ ! "$FILE" =~ ^-.* ]]; then
+    FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+    DEST=${PARSING_ARRAY[1]}
+    if [ -n "$DEST" ]; then
+      FILE=$DEST
+    fi
+    echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
+  fi
+done
+
 (cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
 # Copyright (C) 2015 The Dokdo Project
 #
